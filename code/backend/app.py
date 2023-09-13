6ac3b6bb51a168911907b8ce31ecc9b498f45d08
e18264 <e18264@eng.pdn.ac.lk>
2023-09-13 11:44:53 +0530

    complete signin, login and logout

--- code/backend/app.py
+++ code/backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
-import jwt
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, unset_jwt_cookies
+
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '22e2ea1408a8454eb96aa303d3e29424'
+app.config['JWT_TOKEN_LOCATION'] = ['headers', 'cookies']
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False  # Disable CSRF protection for cookies
 
 # Replace the following with your MongoDB Atlas connection string
 MONGODB_URI = "mongodb+srv://madushan:6ST-x-XUNJ8D_gk@6sp.b6xvqam.mongodb.net/"
@@ -16,8 +19,12 @@ client = MongoClient(MONGODB_URI)
 # Access your MongoDB Atlas database
 db = client['6SP-database']
 
-# Create a collection for users
-users = db['user-details']
+# Create a collection for teachers
+teachers = db['teacherss']
+student = db['students']
+
+# Setup Flask-JWT-Extended
+jwt = JWTManager(app)
 
 # User model
 class User:
@@ -25,7 +32,7 @@ class User:
         self.username = username
         self.password = password
 
-#For testing
+# For testing
 @app.route('/api/test', methods=['POST'])
 def test():
     data = request.get_json()
@@ -38,16 +45,16 @@ def signup():
     data = request.get_json()
     username = data['username']
     password = generate_password_hash(str(data['password']), method='sha256')
-    
+
     if not username or not password:
         return jsonify({'message': 'Username and password are required'}), 400
 
-    existing_user = users.find_one({'username': username})
+    existing_user = teachers.find_one({'username': username})
     if existing_user:
         return jsonify({'message': 'User already exists'}), 400
 
     new_user = User(username, password)
-    users.insert_one(new_user.__dict__)
+    teachers.insert_one(new_user.__dict__)
     return jsonify({'message': 'User created successfully'}), 201
 
 # Create a login route
@@ -57,31 +64,32 @@ def login():
     username = data['username']
     password = data['password']
 
-    user = users.find_one({'username': username})
-    if not user or not check_password_hash(user['password'], password):
+    user = teachers.find_one({'username': username})
+    if not user or not check_password_hash(user['password'], str(password)):
         return jsonify({'message': 'Invalid username or password'}), 401
 
     # Create a JWT token
-    token = jwt.encode({'username': user['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)}, app.config['SECRET_KEY'], algorithm='HS256')
-    return jsonify({'token': token.decode('UTF-8')})
+    access_token = create_access_token(identity=user['username'], expires_delta=datetime.timedelta(hours=1))
+    
+    # Set JWT token as a cookie and return it in the response
+    response = jsonify({'message': 'Login successful'})
+    response.set_cookie('access_token_cookie', access_token, httponly=True)
+    return response, 200
+
+# Create a logout route
+@app.route('/api/logout', methods=['POST'])
+def logout():
+    # Unset the JWT token cookie
+    response = jsonify({'message': 'Logout successful'})
+    unset_jwt_cookies(response)
+    return response, 200
 
 # Create a protected route
 @app.route('/api/protected', methods=['GET'])
+@jwt_required()
 def protected():
-    token = request.headers.get('Authorization')
-
-    if not token:
-        return jsonify({'message': 'Token is missing'}), 401
-
-    try:
-        # Decode the JWT token
-        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        username = decoded_token['username']
-        return jsonify({'message': f'Protected route accessed by {username}'}), 200
-    except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'Token has expired'}), 401
-    except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid token'}), 401
+    username = get_jwt_identity()
+    return jsonify({'message': f'Protected route accessed by {username}'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
