b68a7858f1def2caafe2736662a8be53ee266c0d
sheiiz <shehan2k@gmail.com>
2023-09-14 00:30:04 +0530

    Modify the python codes

--- code/backend/Predictor.py
+++ code/backend/Predictor.py
@@ -74,12 +74,12 @@ def get_model():
     return model
 
 
-def convertToVec(text):
+def predictMarks(text):
     content = text
     if len(content) > 20:
         num_features = 300
         model = KeyedVectors.load_word2vec_format(
-            "Saved_Models/word2vecmodel.bin", binary=True
+            "../../Saved_Models/word2vecmodel.bin", binary=True
         )
         clean_test_essays = []
         clean_test_essays.append(sent2word(content))
@@ -89,12 +89,12 @@ def convertToVec(text):
             testDataVecs, (testDataVecs.shape[0], 1, testDataVecs.shape[1])
         )
 
-        lstm_model = load_model("Saved_Models/final_lstm.h5")
+        lstm_model = load_model("../../Saved_Models/final_lstm.h5")
         preds = lstm_model.predict(testDataVecs)
         return str(round(preds[0][0]))
 
 
 # essay = """ Dear The computer blinked to life and an image of a blonde haired girl filled the screen. It was easy to find out how life was in thanks to the actual girl explaining it. Going to the library wouldn't have filled one with this priceless information and human interection. Computers are a nessessity of life if soceity wishes to grow and expand. They should be supported because they teach hand eye coordination, give people the ability to learn about faraway places, and allow people to talk to others online. Firstly, computers help teach hand eye coordination. Hand-eye coordination is a useful ability that is usod to excel in sports. In a recent survey, of kids felt their hand eye coordination improves after computer use. Even a simple thing like tying can build up this skill. Famous neurologist stated in an article last week that, "@CAPS3 and computer strength the When on the computer, you automatically process what the eyes see into a command for your hands." hand eye coordination can improve people in sports such as baseball and basketball. If someone wan't to become better in these sports, all they'd need to do was turn on the computer. Once people become better at sports, they're more likely to play them and become more healthy. In reality, computers can help with exercising instead of decreasing it. Additionaly, computers allow people to access information about faraway places and people. If someone wanted to reasearch all they'd need to do was type in a search would be presented to them in it would link forever to search through countless things. Also, having the ability to learn about cultures can make peole peole and their cultures, they understand others something. Increase tolerance people are. Computers are a resourceful tool that they can help people in every different aspect of life. Lastly, computer and in technology can allow people to chat. Computer chat and video chat can help the all different nations. Bring on good terms places other than can help us understand story comes out about something that happend in people can just go on their computer and ask an actual citizen their take on the matter. Also, video chat and online conversation can cut down on expensive phone bills. No one wants to pay more than they have to in this economy. Another good point is that you can acess family members you scaresly visit. It can help you connect within your own family more. Oviously, computers are a useful aid in todays era. their advancements push the world foreward to a better place. Computers can help people because they help teach handeye coordination, give people the bility to learn about faraway places and people, and allow people to talk online with others. Think of a world with no computers or technologicall advancements. The world would be sectored and unified, contact between people scare, and information even. The internet is like thousands or librarys put together. Nobody would know much about other nations and news would travel slower. Is that the kind of palce you want people to live in? """
 
-# score = convertToVec(essay)
-# print(score)
+# score = predictMarks(essay)
+# print('The score is ', score)
